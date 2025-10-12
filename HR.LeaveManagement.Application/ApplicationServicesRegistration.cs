bcdcafdb71183f1b5d65e0294127fe18ce40e814
Mohammadreza Hashemi <it93mohammad@gmail.com>
2025-10-12 15:56:20 +0330

    + DTOs was added + AutoMapper was added and used for DTOs

---
+++ HR.LeaveManagement.Application/ApplicationServicesRegistration.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using HR.LeaveManagement.Application.Profiles;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HR.LeaveManagement.Application
+{
+    public static class ApplicationServicesRegistration
+    {
+        public static void ConfigureApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        }
+    }
+}
